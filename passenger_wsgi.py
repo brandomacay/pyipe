730e1dbfbbe7f2f8e2504b25f170ccdfa0468184
Brandon Macay <32371887+brandomacay@users.noreply.github.com>
2024-07-13 15:34:11 -0500

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,14 +1,5 @@
-import importlib.util
-import os
-import sys
+from flask import Flask
+from app import app as application
 
-# Asegúrate de que el directorio actual sea el correcto
-current_dir = os.path.dirname(__file__)
-sys.path.insert(0, current_dir)
-
-# Especifica la ruta completa hacia test.py si es necesario
-wsgi_spec = importlib.util.spec_from_file_location('wsgi', os.path.join(current_dir, 'app.py'))
-wsgi = importlib.util.module_from_spec(wsgi_spec)
-wsgi_spec.loader.exec_module(wsgi)
-
-application = wsgi.app
+if __name__ == '__main__':
+    application.run(host='0.0.0.0', port=5000)
