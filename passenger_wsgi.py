4069a01b689200b950fd3fa053b1cc687f4859ff
Brandon Macay <32371887+brandomacay@users.noreply.github.com>
2024-07-13 15:49:05 -0500

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -2,4 +2,4 @@ from flask import Flask
 from app import app as application
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=False)
+    application.run(host='10.214.203.211', port=5000)
