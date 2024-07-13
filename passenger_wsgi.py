b4a70b7674ba2b9ad68cc7066483ee3ffc5e1ae9
Brandon Macay <32371887+brandomacay@users.noreply.github.com>
2024-07-13 15:29:00 -0500

    Update passenger_wsgi.py

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,9 +1,14 @@
-import imp
+import importlib.util
 import os
 import sys
 
+# Asegúrate de que el directorio actual sea el correcto
+current_dir = os.path.dirname(__file__)
+sys.path.insert(0, current_dir)
 
-sys.path.insert(0, os.path.dirname(__file__))
+# Especifica la ruta completa hacia test.py si es necesario
+wsgi_spec = importlib.util.spec_from_file_location('wsgi', os.path.join(current_dir, 'app.py'))
+wsgi = importlib.util.module_from_spec(wsgi_spec)
+wsgi_spec.loader.exec_module(wsgi)
 
-wsgi = imp.load_source('wsgi', 'app.py')
 application = wsgi.app
