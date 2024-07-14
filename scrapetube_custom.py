34f204859c3b617b526f4f2c49ced3b42d45e4de
Brandon Macay <32371887+brandomacay@users.noreply.github.com>
2024-07-13 23:18:08 -0500

    Update scrapetube_custom.py

--- scrapetube_custom.py
+++ scrapetube_custom.py
@@ -1,26 +1,154 @@
 import json
 import time
 from typing import Generator
+
 import requests
 from typing_extensions import Literal
-from bs4 import BeautifulSoup
 
-results_type_map = {
-    "video": ["B", "videoRenderer"],
-    "channel": ["C", "channelRenderer"],
-    "playlist": ["D", "playlistRenderer"],
-    "movie": ["E", "videoRenderer"],
-    "music": ["F", "musicVideoRenderer"],  # Adjusted for music videos
+type_property_map = {
+    "videos": "videoRenderer",
+    "streams": "videoRenderer",
+    "shorts": "reelItemRenderer"
 }
+
+def get_channel(
+    channel_id: str = None,
+    channel_url: str = None,
+    channel_username: str = None,
+    limit: int = None,
+    sleep: float = 1,
+    proxies: dict = None,
+    sort_by: Literal["newest", "oldest", "popular"] = "newest",
+    content_type: Literal["videos", "shorts", "streams"] = "videos",
+) -> Generator[dict, None, None]:
+
+    """Get videos for a channel.
+
+    Parameters:
+        channel_id (``str``, *optional*):
+            The channel id from the channel you want to get the videos for.
+            If you prefer to use the channel url instead, see ``channel_url`` below.
+
+        channel_url (``str``, *optional*):
+            The url to the channel you want to get the videos for.
+            Since there is a few type's of channel url's, you can use the one you want
+            by passing it here instead of using ``channel_id``.
+
+        channel_username (``str``, *optional*):
+            The username from the channel you want to get the videos for.
+            Ex. ``LinusTechTips`` (without the @).
+            If you prefer to use the channel url instead, see ``channel_url`` above.
+
+        limit (``int``, *optional*):
+            Limit the number of videos you want to get.
+
+        sleep (``int``, *optional*):
+            Seconds to sleep between API calls to youtube, in order to prevent getting blocked.
+            Defaults to 1.
+
+        proxies (``dict``, *optional*):
+            A dictionary with the proxies you want to use. Ex:
+            ``{'https': 'http://username:password@101.102.103.104:3128'}``
+        
+        sort_by (``str``, *optional*):
+            In what order to retrieve to videos. Pass one of the following values.
+            ``"newest"``: Get the new videos first.
+            ``"oldest"``: Get the old videos first.
+            ``"popular"``: Get the popular videos first. Defaults to "newest".
+
+        content_type (``str``, *optional*):
+            In order to get content type. Pass one of the following values.
+            ``"videos"``: Videos
+            ``"shorts"``: Shorts
+            ``"streams"``: Streams
+    """
+
+    base_url = ""
+    if channel_url:
+        base_url = channel_url
+    elif channel_id:
+        base_url = f"https://www.youtube.com/channel/{channel_id}"
+    elif channel_username:
+        base_url = f"https://www.youtube.com/@{channel_username}"
+
+    url = "{base_url}/{content_type}?view=0&flow=grid".format(
+        base_url=base_url,
+        content_type=content_type,
+    )
+    api_endpoint = "https://www.youtube.com/youtubei/v1/browse"
+    videos = get_videos(url, api_endpoint, type_property_map[content_type], limit, sleep, proxies, sort_by)
+    for video in videos:
+        yield video
+
+
+def get_playlist(
+    playlist_id: str, limit: int = None, sleep: int = 1, proxies: dict = None
+) -> Generator[dict, None, None]:
+
+    """Get videos for a playlist.
+
+    Parameters:
+        playlist_id (``str``):
+            The playlist id from the playlist you want to get the videos for.
+
+        limit (``int``, *optional*):
+            Limit the number of videos you want to get.
+
+        sleep (``int``, *optional*):
+            Seconds to sleep between API calls to youtube, in order to prevent getting blocked.
+            Defaults to 1.
+        
+        proxies (``dict``, *optional*):
+            A dictionary with the proxies you want to use. Ex:
+            ``{'https': 'http://username:password@101.102.103.104:3128'}``
+    """
+
+    url = f"https://www.youtube.com/playlist?list={playlist_id}"
+    api_endpoint = "https://www.youtube.com/youtubei/v1/browse"
+    videos = get_videos(url, api_endpoint, "playlistVideoRenderer", limit, sleep, proxies)
+    for video in videos:
+        yield video
+
 
 def get_search(
     query: str,
     limit: int = None,
     sleep: int = 1,
     sort_by: Literal["relevance", "upload_date", "view_count", "rating"] = "relevance",
-    results_type: Literal["video", "channel", "playlist", "movie", "music"] = "video",
+    results_type: Literal["video", "channel", "playlist", "movie"] = "video",
     proxies: dict = None,
 ) -> Generator[dict, None, None]:
+
+    """Search youtube and get videos.
+
+    Parameters:
+        query (``str``):
+            The term you want to search for.
+
+        limit (``int``, *optional*):
+            Limit the number of videos you want to get.
+
+        sleep (``int``, *optional*):
+            Seconds to sleep between API calls to youtube, in order to prevent getting blocked.
+            Defaults to 1.
+
+        sort_by (``str``, *optional*):
+            In what order to retrieve to videos. Pass one of the following values.
+            ``"relevance"``: Get the new videos in order of relevance.
+            ``"upload_date"``: Get the new videos first.
+            ``"view_count"``: Get the popular videos first.
+            ``"rating"``: Get videos with more likes first.
+            Defaults to "relevance".
+
+        results_type (``str``, *optional*):
+            What type you want to search for. Pass one of the following values:
+            ``"video"|"channel"|"playlist"|"movie"``. Defaults to "video".
+        
+        proxies (``dict``, *optional*):
+            A dictionary with the proxies you want to use. Ex:
+            ``{'https': 'http://username:password@101.102.103.104:3128'}``
+
+    """
 
     sort_by_map = {
         "relevance": "A",
@@ -29,43 +157,48 @@ def get_search(
         "rating": "E",
     }
 
-    param_string = f"CA{sort_by_map[sort_by]}SAhA{results_type_map[results_type][0]}"
-    if results_type in ["playlist", "music"]:
-        return get_playlist_or_music_search(query, limit, sleep, sort_by, proxies, results_type_map[results_type][1])
-    else:
-        url = f"https://www.youtube.com/results?search_query={query}&sp={param_string}"
-        api_endpoint = "https://www.youtube.com/youtubei/v1/search"
-        videos = get_videos(
-            url, api_endpoint, results_type_map[results_type][1], limit, sleep, proxies
-        )
-        for video in videos:
-            yield video
-
-
-def get_playlist_or_music_search(query, limit, sleep, sort_by, proxies, selector):
-    sort_by_map = {
-        "relevance": "relevance",
-        "upload_date": "upload_date",
-        "view_count": "view_count",
-        "rating": "rating",
+    results_type_map = {
+        "video": ["B", "videoRenderer"],
+        "channel": ["C", "channelRenderer"],
+        "playlist": ["D", "playlistRenderer"],
+        "movie": ["E", "videoRenderer"],
     }
 
-    url = f"https://www.youtube.com/results?search_query={query}&sp=EgIQAQ%253D%253D"  # Music filter
-    response = requests.get(url, proxies=proxies)
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.content, "html.parser")
-        items = soup.find_all("ytd-video-renderer")
-        for item in items[:limit]:
-            video_data = {
-                "videoId": item.find("a", class_="yt-simple-endpoint style-scope ytd-video-renderer")["href"].split("=", 1)[1],
-                "title": item.find("yt-formatted-string", class_="style-scope ytd-video-renderer").text.strip(),
-                "channel_name": item.find("yt-formatted-string", class_="style-scope ytd-channel-name complex-string").text.strip(),
-                "published_time": item.find("yt-formatted-string", class_="style-scope ytd-video-renderer").text.strip(),
-                "views": item.find("span", class_="style-scope ytd-video-meta-block").text.strip(),
-                "thumbnail": item.find("img", class_="style-scope yt-img-shadow")["src"],
-                "description": item.find("yt-formatted-string", class_="style-scope ytd-video-renderer").text.strip(),
-            }
-            yield video_data
+    param_string = f"CA{sort_by_map[sort_by]}SAhA{results_type_map[results_type][0]}"
+    url = f"https://www.youtube.com/results?search_query={query}&sp={param_string}"
+    api_endpoint = "https://www.youtube.com/youtubei/v1/search"
+    videos = get_videos(
+        url, api_endpoint, results_type_map[results_type][1], limit, sleep, proxies
+    )
+    for video in videos:
+        yield video
+
+
+
+def get_video(
+    id: str,
+) -> dict:
+
+    """Get a single video.
+
+    Parameters:
+        id (``str``):
+            The video id from the video you want to get.
+    """
+
+    session = get_session()
+    url = f"https://www.youtube.com/watch?v={id}"
+    html = get_initial_data(session, url)
+    client = json.loads(
+        get_json_from_html(html, "INNERTUBE_CONTEXT", 2, '"}},') + '"}}'
+    )["client"]
+    session.headers["X-YouTube-Client-Name"] = "1"
+    session.headers["X-YouTube-Client-Version"] = client["clientVersion"]
+    data = json.loads(
+        get_json_from_html(html, "var ytInitialData = ", 0, "};") + "}"
+    )
+    return next(search_dict(data, "videoPrimaryInfoRenderer"))
+
 
 
 def get_videos(
@@ -89,7 +222,7 @@ def get_videos(
             )
             next_data = get_next_data(data, sort_by)
             is_first = False
-            if sort_by and sort_by != "relevance": 
+            if sort_by and sort_by != "newest": 
                 continue
         else:
             data = get_ajax_data(session, api_endpoint, api_key, next_data, client)
@@ -153,13 +286,13 @@ def get_json_from_html(html: str, key: str, num_chars: int = 2, stop: str = '"')
 
 
 def get_next_data(data: dict, sort_by: str = None) -> dict:
+    # Youtube, please don't change the order of these
     sort_by_map = {
-        "relevance": 0, 
-        "upload_date": 1,
-        "view_count": 2, 
-        "rating": 3
+        "newest": 0, 
+        "popular": 1,
+        "oldest": 2, 
     }
-    if sort_by and sort_by != "relevance":
+    if sort_by and sort_by != "newest":
         endpoint = next(
             search_dict(data, "feedFilterChipBarRenderer"), None)["contents"][sort_by_map[sort_by]]["chipCloudChipRenderer"]["navigationEndpoint"]
     else:
